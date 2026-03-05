6285242511b3800a6746ee18bf1b6fb2748462f9
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2026-03-05 18:45:14 -0300

    Create run.sh

---
+++ v4/multiendoscopia/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+IMAGE="tonanuvem/sisprime-multi-banco-app"
+CONTAINER="sisprime-multi-banco-agent-app"
+PORTA="8802"
+
+echo "Building Docker image with direct pip install..."
+docker build -f Dockerfile -t $IMAGE  .
+
+echo "Image size:"
+docker images $IMAGE
+
+echo "Starting container..."
+docker run -d \
+  --name $CONTAINER \
+  -p $PORTA:8501 \
+  -v $(pwd)/logs:/app/logs \
+  -v $(pwd)/outputs:/app/outputs \
+  $IMAGE
+
+echo "Running... porta: $PORTA"
